64975ba798565e974f7ec14e928112db3c598b4b
kaiba <kaibadash@gmail.com>
2020-04-20 01:53:01 +0900

    :bug: NOTE_TYPE_16TH's duration should be 0.5

--- src/main/kotlin/com/pokosho/midi2musicxml/BaseNote.kt
+++ src/main/kotlin/com/pokosho/midi2musicxml/BaseNote.kt
@@ -1,6 +1,6 @@
 package com.pokosho.midi2musicxml
 
-open class BaseNote(start: Int, end: Int = 0) {
+abstract class BaseNote(start: Int, end: Int = 0) {
   var start: Int = 0
   var end: Int = 0
   var noteType: NoteType = NoteType.NOTE_TYPE_QUARTER
