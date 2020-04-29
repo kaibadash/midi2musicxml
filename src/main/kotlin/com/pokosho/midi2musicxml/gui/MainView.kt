f167d3c06880f478a61d5ce60d59509024db45c2
kaiba <kaibadash@gmail.com>
2020-04-29 15:17:34 +0900

    Bind from GUI to code

--- src/main/kotlin/com/pokosho/midi2musicxml/gui/MainView.kt
+++ src/main/kotlin/com/pokosho/midi2musicxml/gui/MainView.kt
@@ -2,17 +2,27 @@ package com.pokosho.midi2musicxml.gui
 
 import javafx.scene.control.Button
 import javafx.scene.control.TextArea
+import javafx.scene.control.TextField
 import javafx.scene.layout.AnchorPane
 import tornadofx.*
 
 class MainView : View("MainView") {
   override val root: AnchorPane by fxml("/fxml/main_view.fxml")
-  private val button: Button by fxid("sampleButton")
-  private val textArea: TextArea by fxid("sampleTextArea")
+  private val buttonLazySearch: Button by fxid("buttonLazySearch")
+  private val buttonSelectNeutrino: Button by fxid("buttonSelectNeutrino")
+  private val buttonSelectMidi: Button by fxid("buttonSelectMidi")
+  private val buttonSelectLyric: Button by fxid("buttonSelectLyric")
+  private val buttonPreview: Button by fxid("buttonPreview")
+  private val buttonGenerate: Button by fxid("buttonGenerate")
+  private val textPathToNeutrino: TextField by fxid("textPathToNeutrino")
+  private val textPathToInputMid: TextField by fxid("textPathToInputMid")
+  private val textPathToLyric: TextField by fxid("textPathToLyric")
+  private val textPreview: TextArea by fxid("textPreview")
+  private val textMessage: TextArea by fxid("textMessage")
 
   init {
-    button.action {
-      textArea.text = "Hello TornadoFX by FXML"
+    buttonLazySearch.action {
+      textMessage.text = "Hello TornadoFX by FXML"
     }
   }
 }
--- src/main/kotlin/com/pokosho/midi2musicxml/gui/Midi2MusicXMLGUI.kt
+++ src/main/kotlin/com/pokosho/midi2musicxml/gui/Midi2MusicXMLGUI.kt
@@ -2,4 +2,4 @@ package com.pokosho.midi2musicxml.gui
 
 import tornadofx.App
 
-class MyApp: App(MainView::class, Styles::class)
+class Midi2MusicXMLGUI: App(MainView::class, Styles::class)
