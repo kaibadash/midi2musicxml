3f5642783a93b4b6208699bfd31cbfdde93f57af
kaiba <kaibadash@gmail.com>
2020-05-13 01:59:03 +0900

    WIP

--- src/main/kotlin/com/pokosho/midi2musicxml/executor/NeutrinoExecutor.kt
+++ src/main/kotlin/com/pokosho/midi2musicxml/executor/NeutrinoExecutor.kt
@@ -2,10 +2,14 @@ package com.pokosho.midi2musicxml.executor
 
 import org.apache.log4j.Logger
 import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
+import java.nio.file.Files
+import java.nio.file.Paths
 
 class NeutrinoExecutor(val dirNuetrino: String, val pathToMusicXML: String,
                        val singer: Singer = Singer.Kiritan,
-                       val pitchShift: Double = 1.0, val formantShift: Double = 1.0) {
+                       val pitchShift: Double = 1.0, val formantShift: Double = 1.0, val outputStream: OutputStreamt) {
   private val log: Logger = Logger.getLogger(NeutrinoExecutor::class.java)
 
   /**
@@ -13,44 +17,39 @@ class NeutrinoExecutor(val dirNuetrino: String, val pathToMusicXML: String,
    */
   fun execute(): String {
     val target = baseFileName(pathToMusicXML)
-
     val labelFull = "${target}-full.lab"
     val labelMono = "${target}-mono.lab"
-    var builder = ProcessBuilder("${dirNuetrino}/bin/musicXMLtoLabel",
-      "${target}.musicxml", labelFull, labelMono)
-    val thread = Runtime.getRuntime().availableProcessors().toString()
-    builder.directory(File(dirNuetrino))
-    builder.inheritIO()
-    log.debug(builder.command().joinToString(" "))
-    var process = builder.start()
-    process.waitFor()
+    executeCommand("${dirNuetrino}/bin/musicXMLtoLabel",
+            "${target}.musicxml", labelFull, labelMono)
 
+    val thread = Runtime.getRuntime().availableProcessors().toString()
     val labelTiming = "${target}-timing.lab"
     val f0 = "${target}.f0"
     val mgc = "${target}.mgc"
     val bap = "${target}.bap"
-    builder = ProcessBuilder("${dirNuetrino}/bin/NEUTRINO",
-      labelFull, labelTiming, f0, mgc, bap,
-      "./model/${singer}/",
-      "-n", thread, "-t"
-    )
-    builder.directory(File(dirNuetrino))
-    builder.inheritIO()
-    log.debug(builder.command().joinToString(" "))
-    process = builder.start()
-    process.waitFor()
+    executeCommand("${dirNuetrino}/bin/NEUTRINO",
+            labelFull, labelTiming, f0, mgc, bap,
+            "./model/${singer}/",
+            "-n", thread, "-t")
 
     val output = "${target}.wav"
-    builder = ProcessBuilder("${dirNuetrino}/bin/WORLD",
-      f0, mgc, bap, "-f", pitchShift.toString(), "-m", formantShift.toString(),
-      "-o", output, "-n", thread, "-t"
-    )
-    builder.directory(File(dirNuetrino))
-    builder.inheritIO()
-    log.debug(builder.command().joinToString(" "))
-    process = builder.start()
-    process.waitFor()
+    executeCommand("${dirNuetrino}/bin/WORLD",
+            f0, mgc, bap, "-f", pitchShift.toString(), "-m", formantShift.toString(),
+            "-o", output, "-n", thread, "-t")
+
     return output
+  }
+
+  private fun executeCommand(vararg commandWithArgs: String) {
+    val builder = ProcessBuilder(*commandWithArgs)
+    builder.directory(File(dirNuetrino))
+
+    builder.environment().putIfAbsent("DYLD_LIBRARY_PATH", "${dirNuetrino}/bin")
+    log.debug(builder.command().joinToString(" "))
+    val process = builder.start()
+    process.inputStream.transferTo(outputStream)
+    process.waitFor()
+    if (process.exitValue() > 0) 
   }
 
   /**
